b2236ff6d85a723e805010ba532a7aee00ccbf02
mroger <rene.mroger@gmail.com>
2019-07-08 22:10:00 +0000

    server updatte

--- express_server.js
+++ express_server.js
@@ -6,12 +6,26 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const a = 1;
+const b = 2;
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${b}`);
 });
 
 app.listen(PORT, () => {
