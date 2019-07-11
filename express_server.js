6c0a0cc478a02244bde9c4dd1244e04712ae76ac
mroger <rene.mroger@gmail.com>
2019-07-11 19:33:58 +0000

    hashed cookies

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const generateRandomString = require('./utils/utils');
 const { users } = require('./users');
 const { validateEmail, validatePassword } = require('./validations');
@@ -11,21 +11,31 @@ const urlDatabase = require('./data');
 const bcrypt = require('bcrypt');
 
 
+app.set('trust proxy', 1)
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
+app.set('trust proxy', 1)
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
 
 
 app.get("/", (request, response) => {
+
+  request.session.views = (req.session.views || 0) + 1
   response.send(`<html><body>
   <a href="/urls">link text</a>
   </body></hmtl>`);
 });
 
 app.get("/urls", (request, response) => {
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session['user_id'];
   let templateVars = {
     urls: urlDatabase,
     user: getUserById(user_id)
@@ -40,26 +50,26 @@ app.get("/u/:shortURL", (request, response) => {
 
 app.get("/login", (request, response) => {
 
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session.user_id || '';
   let templateVars = { user: getUserById(user_id) }
   response.render('login', templateVars)
 });
 
 
 app.get("/urls/new", (request, response) => {
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session['user_id'];
   let templateVars = { user: getUserById(user_id) };
   response.render("urls_new", templateVars);
 });
 
 app.get("/registration", (request, response) => {
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session['user_id'];
   let templateVars = { user: getUserById(user_id) };
   response.render("registration", templateVars);
 });
 
 app.get("/urls/:shortURL", (request, response) => {
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session['user_id'];
   let templateVars = { user: getUserById(user_id), shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL].longURL };
   response.render("urls_show", templateVars);
 });
@@ -69,7 +79,7 @@ app.post("/urls", (request, response) => {
   //IF USER IS LOGGED IN
   urlDatabase[key] = {
     longURL: request.body.longURL,
-    userID: request.cookies["user_id"]
+    userID: request.session['user_id']
   }
   response.redirect("urls");       // responsepond with 'Ok' (we will replace this)
 });
@@ -81,30 +91,27 @@ app.post("/login", (request, response) => {
   for (const key in users) {
     if (users[key].email === userEmail &&
       bcrypt.compareSync(userPassword, users[key].password)) {
-      response.cookie('user_id', users[key].id);
+      request.session.user_id = users[key].id;
       response.redirect('/urls');
       return;
     }
   }
   response.status(403).send("Server Error: 403");
-
 });
 
 app.post("/logout", (request, response) => {
-  response.clearCookie("user_id");
+  request.session = null
   response.redirect('/urls');
 });
 
 app.post("/urls/:shortURL/updatedLong", (request, response) => {
   //updating longURL  
-  console.log(request.body)
   urlDatabase[request.params.shortURL].longURL = request.body.updatedLong;
   response.redirect('/urls');
 })
 
 app.post("/urls/:shortURL/delete", (request, response) => {
-  console.log(urlDatabase[request.params.shortURL]);
-  if (urlDatabase[request.params.shortURL].userID === request.cookies["user_id"]) {
+  if (urlDatabase[request.params.shortURL].userID === request.session['user_id']) {
     delete urlDatabase[request.params.shortURL];
   }
   response.redirect('/urls');
@@ -117,13 +124,13 @@ app.post("/registration", validatePassword, validateEmail, (request, response) =
     email: request.body.email,
     password: bcrypt.hashSync(request.body.password, 10)
   }
-  response.cookie('user_id', randId);
+  request.session.user_id = randId;
   users[randId] = user;
   response.redirect('/urls');
 })
 
 app.use((error, request, response, next) => {
-  const user_id = request.cookies["user_id"];
+  const user_id = request.session['user_id'];
   let templateVars = { user: getUserById(user_id), error: error.error };
   if (error && error.statusCode) {
     response
